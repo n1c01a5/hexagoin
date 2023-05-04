35f0166f4cff10d9ce04015181f3275cadff0f5b
Nicolas Wagner <contact@wagner-nicolas.com>
2023-05-04 14:24:19 +0200

    feat: add smart contract

---
+++ hexagoin.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+pragma solidity 0.8.19;
+
+contract Hexagoin {
+  string name = "Hexacoin";
+  string symbol = "HCN";
+
+  constructor() {
+    
+  }
+}
